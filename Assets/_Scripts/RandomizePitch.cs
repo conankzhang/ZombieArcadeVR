3213669cf6769a26ef37d8cc88536df03b220f38
conankzhang <u0409453@utah.edu>
2017-12-01 12:01:30 -0700

    Pushing ZombieArcadeVR to GitHub

---
+++ Assets/_Scripts/RandomizePitch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizePitch : MonoBehaviour {
+    AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.pitch = Random.Range(0.7f, 0.8f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/SimpleAI.cs
@@ -0,0 +1,41 @@
+﻿ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.UI;
+
+ public class SimpleAI: MonoBehaviour
+ {
+ 
+     private Transform Player;
+
+     [SerializeField]
+     private int MoveSpeed = 4;
+     int MaxDist = 10;
+
+     [SerializeField]
+     private int minimumDistance = 5;
+ 
+     void Start()
+     {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+     }
+ 
+     void Update()
+     {
+         transform.LookAt(Player);
+ 
+         if (Vector3.Distance(transform.position, Player.position) >= minimumDistance)
+         {
+ 
+             transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+ 
+ 
+ 
+             if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+             {
+                 //Here Call any function U want Like Shoot at here or something
+             }
+ 
+         }
+     }
+ }
